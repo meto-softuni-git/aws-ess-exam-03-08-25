a87071bfcf4b59f25ee22ea1414c8b1e17a4f5a6
metodil <metodil@hotmail.com>
2025-08-07 10:24:19 +0300

    fix version.tf use_lockfile

--- terraform/version.tf
+++ terraform/version.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    region = "eu-central-1"
+    region         = "eu-central-1"
     bucket         = "softuni-meto-terraform-states"
     key            = "envs/aws-examp/exam-03-08-25.tfstate"
     encrypt        = true
-    dynamodb_table = "terraform-locks"
+    use_lockfile   = "terraform-locks"
   }
 }
